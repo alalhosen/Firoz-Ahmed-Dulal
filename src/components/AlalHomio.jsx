cf9f69ddf34926a06711ede87d9c3906018522b5
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-22 21:03:51 +0600

    homio medicine list

--- src/components/AlalHomio.jsx
+++ src/components/AlalHomio.jsx
@@ -2,15 +2,40 @@
 const AlalHomio = () => {
     return (
         <>
-          <div className="mt-24">
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            </div>  
+            <div className="mt-24">
+                <ul>
+                    <h2 className="text-xl text-red-500 mt-4">A. 03.09.24--->Tk-300</h2>
+                    <li>1. Acid Nit</li>
+                    <li>2. Aesculas hippocastanum  CM</li>
+                    <li>3. Thuja</li>
+                    <li>4. Cap. Demorhoid-500mg = 20pis</li>
+                    <li>5. Drop. Piles Cure</li>
+                    <h2 className="text-xl text-red-500 mt-4">B. 15.09.24--->Tk-100</h2>
+                    <li>1. Acid Nit</li>
+                    <li>2. Aesculas hippocastanum  CM</li>
+                    <li>3. Thuja</li>
+                    <li>4. Cap. ........-500mg = 10pis</li>
+                    <li>5. Drop. Piles Cure</li>
+                    <h2 className="text-xl text-red-500 mt-4">C. 25.09.24--->Tk-.....</h2>
+                    <li>1. Acid Nit</li>
+                    <li>2. Aesculas hippocastanum  CM</li>
+                    <li>3. Thuja</li>
+                    <li>4. Cap. Demorhoid-500mg = 20pis</li>
+                    <li>5. Drop. Piles Cure</li>
+                    <h2 className="text-xl text-red-500 mt-4">D. 09.11.24--->Tk-270</h2>
+                    <li>1. Acid Nit</li>
+                    <li>2. Aesculas hippocastanum  CM</li>
+                    <li>3. Thuja</li>
+                    <li>4. Cap. Demorhoid-500mg = 10pis</li>
+                    <li>5. Drop. Kent 7</li>
+                    <h2 className="text-xl text-red-500 mt-4">E. 17.11.24--->Tk-150</h2>
+                    <li>1. Conium mac 30</li>
+                    <li>2. Nux vomica 200</li>
+                    <li>3. Aesculas hippocastanum  CM</li>
+                    <li>4. Cap. Demorhoid-500mg = 20pis</li>
+                    <li>5. Drop. Kent 7</li>
+                </ul>
+            </div>
         </>
     );
 };
