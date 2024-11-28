0c976fc3d9544fb06c60fa808ea03078a73ef458
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-28 11:07:27 +0600

    img change

--- src/components/BannerBlood.jsx
+++ src/components/BannerBlood.jsx
@@ -2,20 +2,22 @@
 const BannerBlood = () => {
     return (
         <>
-       
-            <div className="text-center mt-20">
-                <h1 className="text-2xl font-bold text-black">ডায়াবেটিকস ও রক্তের গ্রুপ পরীক্ষা করা হয়।</h1>
-                <h2 className="my-5 font-extrabold text-xl text-black">০ দিন থেকে বাচ্চাদের কেনোলা ও গ্যাস দেওয়া হয়।</h2>
-                <h3 className="text-orange-400 pb-4 underline text-black font-extrabold text-xl ">স্থানঃ ভোমরাঘাট বাজার।</h3>
+
+            <div className="mt-20 text-center">
+                <h1 className="font-bold text-2xl text-black">ডায়াবেটিকস ও রক্তের গ্রুপ পরীক্ষা করা হয়।</h1>
+                <h2 className="my-5 font-extrabold text-black text-xl">০ দিন থেকে বাচ্চাদের <span className="text-blue-700">কেনোলা ও নেবুলাইজার ( গ্যাস ) </span> দেওয়া হয়।</h2>
+                <h3 className="pb-4 font-extrabold text-black text-orange-400 text-xl underline">স্থানঃ ভোমরাঘাট বাজার।</h3>
                 <h4 className="font-extrabold text-black text-xl">ডাঃ মোঃ ফিরোজ আহমেদ</h4>
                 <h4 className="py-2 font-extrabold text-black">এফ. টি. সি ( আঃ সঃ হাঃ ঠাকুরগাঁও )।</h4>
                 <h5 className="font-extrabold text-black">ডি. এম. এফ ( ঢাকা )।</h5>
             </div>
-            <div className="flex mt-16 justify-center items-center">
-                <img className="w-48 h-48 rounded-xl" src="https://i.ibb.co.com/6gNdPGQ/Banner1.jpg" alt="" />
-                <img className="w-48 h-48 px-2 rounded-xl" src="https://i.ibb.co.com/Rj446mH/Banner3-Blood2.jpg" alt="" />
+            <div className="flex justify-center items-center mt-16">
+                <img className="rounded-xl w-48 h-48" src="https://i.ibb.co.com/6gNdPGQ/Banner1.jpg" alt="" />
+                <img className="px-2 rounded-xl w-48 h-48 nabulaijar2" src="https://i.ibb.co.com/MZDkh6M/Nabulaij2.jpg" alt="" />
 
-                <img className="w-48 h-48 rounded-xl" src="https://i.ibb.co.com/pL410zy/Banner4-Blood3.jpg
+                <img className="bg-nabulaijar px-2 rounded-xl w-24 h-24" src="https://i.ibb.co.com/pd0NmV9/Nabulaij.jpg" alt="" />
+
+                <img className="rounded-xl w-48 h-48" src="https://i.ibb.co.com/pL410zy/Banner4-Blood3.jpg
 " alt="" />
             </div>
 
