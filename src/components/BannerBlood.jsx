1f3a9cb21dfde1d88eeb09166b0a5365f805625c
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-29 12:28:30 +0600

    change css

--- src/components/BannerBlood.jsx
+++ src/components/BannerBlood.jsx
@@ -2,23 +2,26 @@
 const BannerBlood = () => {
     return (
         <>
+            <div className="w-[1400px]">
 
-            <div className="mt-20 text-center">
-                <h1 className="font-bold text-2xl text-black">ডায়াবেটিকস ও রক্তের গ্রুপ পরীক্ষা করা হয়।</h1>
-                <h2 className="my-5 font-extrabold text-black text-xl">০ দিন থেকে বাচ্চাদের <span className="text-blue-700">কেনোলা ও নেবুলাইজার ( গ্যাস ) </span> দেওয়া হয়।</h2>
-                <h3 className="pb-4 font-extrabold text-black text-orange-400 text-xl underline">স্থানঃ ভোমরাঘাট বাজার।</h3>
-                <h4 className="font-extrabold text-black text-xl">ডাঃ মোঃ ফিরোজ আহমেদ</h4>
-                <h4 className="py-2 font-extrabold text-black">এফ. টি. সি ( আঃ সঃ হাঃ ঠাকুরগাঁও )।</h4>
-                <h5 className="font-extrabold text-black">ডি. এম. এফ ( ঢাকা )।</h5>
-            </div>
-            <div className="flex justify-center items-center mt-16">
-                <img className="rounded-xl w-48 h-48" src="https://i.ibb.co.com/6gNdPGQ/Banner1.jpg" alt="" />
-                <img className="px-2 rounded-xl w-48 h-48 nabulaijar2" src="https://i.ibb.co.com/MZDkh6M/Nabulaij2.jpg" alt="" />
+                <div className="mt-28 text-center">
+                    <h1 className="font-bold text-5xl text-black">ডায়াবেটিকস ও রক্তের গ্রুপ পরীক্ষা করা হয়।</h1>
+                    <h2 className="my-28 font-extrabold text-3xl text-black">০ দিন থেকে বাচ্চাদের <span className="text-4xl text-blue-700">কেনোলা ও নেবুলাইজার ( গ্যাস ) </span> দেওয়া হয়।</h2>
+                    <h3 className="pb-32 font-extrabold text-7xl text-black text-orange-400 underline">স্থানঃ ভোমরাঘাট বাজার।</h3>
+                    <h4 className="font-extrabold text-7xl text-black">ডাঃ মোঃ ফিরোজ আহমেদ</h4>
+                    <h4 className="py-16 pt-20 font-extrabold text-6xl text-black">এফ. টি. সি ( আঃ সঃ হাঃ ঠাকুরগাঁও )।</h4>
+                    <h5 className="font-extrabold text-6xl text-black">ডি. এম. এফ ( ঢাকা )।</h5>
+                </div>
 
-                <img className="bg-nabulaijar px-2 rounded-xl w-24 h-24" src="https://i.ibb.co.com/pd0NmV9/Nabulaij.jpg" alt="" />
+                <div className="flex justify-center items-center mt-48">
+                    <img className="rounded-xl w-64 h-64" src="https://i.ibb.co.com/6gNdPGQ/Banner1.jpg" alt="" />
+                    <img className="px-2 rounded-xl w-64 h-64 nabulaijar2" src="https://i.ibb.co.com/MZDkh6M/Nabulaij2.jpg" alt="" />
 
-                <img className="rounded-xl w-48 h-48" src="https://i.ibb.co.com/pL410zy/Banner4-Blood3.jpg
+                    <img className="bg-nabulaijar px-2 rounded-xl w-40 h-40" src="https://i.ibb.co.com/pd0NmV9/Nabulaij.jpg" alt="" />
+
+                    <img className="rounded-xl w-64 h-64" src="https://i.ibb.co.com/pL410zy/Banner4-Blood3.jpg
 " alt="" />
+                </div>
             </div>
 
         </>
